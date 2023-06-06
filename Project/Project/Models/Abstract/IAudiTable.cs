9361c21f391657b49ae7fba108c7a76f3c140cfb
phu <phu3365@gmail,com>
2023-06-06 13:51:05 +0700

    1 nữa database

---
+++ Project/Project/Models/Abstract/IAudiTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models.Abstract
+{
+    public interface IAudiTable
+    {
+        DateTime CreatedDate { get; set; }
+        string CreateBy { get; set; }
+
+        DateTime UpdateDate { get; set; }
+
+        string UpdateBy { get; set; }
+        string MetaKeyWord { get; set; }
+        string MetaDescription { get; set; }
+        bool Status { get; set; }
+    }
+}
---
+++ Project/Project/Models/Domain/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models.Domain
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string CustomerName { get; set; }
+
+        [Required]
+        public string CustomerAddress { get; set; }
+
+        public string CustomerEmail { get; set; }
+
+        public string CustomerMobile { get; set; }
+
+        public string CustomerMessage { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string CreateBy { get; set; }
+
+        public DateTime Update { get; set; }
+
+        public string UpdateBy { get; set; }
+        public string PaymentMethod { get; set; }
+
+        public string PaymentStatus { get; set; }
+
+        public bool Status { get; set; }
+        [ForeignKey("ID")]
+        public virtual OrderDetails OrderDetails { get; set; }
+    }
+}
